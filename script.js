bfbf23a4346e370a749c09393d1654850729bb2e
Lajwanti Harani <106485355+Lajwantiharani@users.noreply.github.com>
2022-10-30 01:26:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,4 @@ const panels = document.querySelectorAll('.panel');
     }
 
     panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));}
